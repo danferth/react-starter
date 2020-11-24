a5b84c6c7bb0f4ca8efa02d7dbaf099036978ce2
danferth <danferth@gmail.com>
2020-11-24 11:33:03 -0800

    initial commit set up tailwindcss

---
+++ src/util.js
@@ -0,0 +1,32 @@
+export const printToday = () => {
+  const date = new Date();
+  const day = date.getDay();
+  let today = "";
+  switch (day) {
+    case 0:
+      today = "Sunday";
+      break;
+    case 1:
+      today = "Monday";
+      break;
+    case 2:
+      today = "Tuesday";
+      break;
+    case 3:
+      today = "Wednesday";
+      break;
+    case 4:
+      today = "Thursday";
+      break;
+    case 5:
+      today = "Friday";
+      break;
+    case 6:
+      today = "Saturday";
+      break;
+    default:
+      break;
+  }
+
+  return today;
+};
